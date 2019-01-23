6769423d789acc6bcf328ff8a5f663a21f02b51e
genxhero <genxhero@users.noreply.github.com>
2019-01-23 09:15:06 -0800

    Initial commit

---
+++ src/div_thingy.jsx
@@ -0,0 +1,29 @@
+import React, { Component } from 'react'
+
+class DivThingy extends Component {
+
+    constructor(props){
+        super(props);
+        this.state ={
+            clicks: 0
+        }
+        this.clickCount = this.clickCount.bind(this)
+    }
+
+    clickCount(){
+        this.setState({
+            clicks: this.state.clicks + 1
+        })
+    }
+
+  render() {
+    return (
+      <div className="fun-div" onClick={this.clickCount}>
+        <h2>Click me, I do things.</h2>
+        <h2>{this.state.clicks} times clicked</h2>
+      </div>
+    )
+  }
+}
+
+export default DivThingy
